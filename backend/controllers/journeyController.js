1b65b5be07020578d6f7b3d510c519289b421f57
Miko Peltokorpi <miko.peltokorpi@gmail.com>
2022-08-06 13:44:27 +0300

    Install express-async-handler

--- backend/controllers/journeyController.js
+++ backend/controllers/journeyController.js
@@ -1,17 +1,17 @@
+const asyncHandler = require('express-async-handler')
 
-
-const getJourneys = (req, res) => {
+const getJourneys = asyncHandler(async (req, res) => {
     res.status(200).json({ message: 'Get journeys'})
-}
+})
 
-const setJourneys = (req, res) => {
+const setJourneys = asyncHandler(async (req, res) => {
     if(!req.body.text) {
         res.status(400)
         throw new Error("Please add a journey")
     }
 
     res.status(200).json({ message: 'Set journeys'})
-}
+})
 
 
 module.exports = {
