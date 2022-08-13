3a3a3e92475fceeab5b7eeff6c2577fd8db1f334
Miko Peltokorpi <miko.peltokorpi@gmail.com>
2022-08-13 23:25:17 +0300

    Attempt to deploy on Heroku

--- backend/server.js
+++ backend/server.js
@@ -25,10 +25,10 @@ app.use("/api/search", require("./routes/search.js"));
 
 //Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('./frontend/build'))
+    app.use(express.static('../frontend/build'))
 
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve('./frontend/build/index.html'))
+        res.sendFile(path.resolve('../frontend/build/index.html'))
     })
 }
 
