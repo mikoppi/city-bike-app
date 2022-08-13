bc94cbe40c1a29608bd7394efc4c0b72512beb57
Miko Peltokorpi <miko.peltokorpi@gmail.com>
2022-08-13 21:25:06 +0300

    Add styling

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -5,14 +5,14 @@ const Home = () => {
   return (
     <div className="home">
       <Link className="home-journeys" to='/journeys'>
-        <div>
+        
           <h1>Journeys</h1>
-        </div>
+        
       </Link>
       <Link className="home-stations" to='/stations'>
-        <div >
+        
           <h1>Stations</h1>
-        </div>
+        
       </Link>
     </div>
   );
